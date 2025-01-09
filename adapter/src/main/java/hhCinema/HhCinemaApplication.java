88212e9feaa9fa6b3542448510608445a4b7dcf2
gwkim <gwkcareer@gmail.com>
2025-01-09 23:35:59 +0900

    create module

---
+++ adapter/src/main/java/hhCinema/HhCinemaApplication.java
@@ -0,0 +1,13 @@
+package hhCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhCinemaApplication.class, args);
+	}
+
+}
